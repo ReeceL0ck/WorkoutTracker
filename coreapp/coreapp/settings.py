a116a1c203627bed9c31b8ea19954d351848e9b5
Reece Lock <reece.lock55@gmail.com>
2025-08-08 11:01:04 +0100

    Added postgres connection + env example

--- coreapp/coreapp/settings.py
+++ coreapp/coreapp/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,11 +78,16 @@ WSGI_APPLICATION = 'coreapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':os.environ.get('NAME'),
+        'USER':os.environ.get('USER'),
+        'PASSWORD':os.environ.get('PASSWORD'),
+        'HOST':os.environ.get('HOST'),
+        'PORT':os.environ.get('PORT','5432')
+
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
